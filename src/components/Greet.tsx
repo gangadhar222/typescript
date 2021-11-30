29df1e6544ea32f62312d32c4c4b74baa4a79888
gangadhar222 <gangadhar.eee@hotmail.com>
2021-11-30 08:32:14 +0530

    changes

--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,11 +1,14 @@
 type GreetProps = {
   name: string;
+  messageCount: number;
 };
 
 export default function Greet(props: GreetProps) {
   return (
     <div>
-      <h2>Hi {props.name}, you have 10 unread messages</h2>
+      <h2>
+        Hi {props.name}, you have {props.messageCount} unread messages
+      </h2>
     </div>
   );
 }
